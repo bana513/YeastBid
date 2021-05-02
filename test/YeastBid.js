77a34b020d0649121dc34d0b8c8c686ca0a1edaf
Tuby Matthew <tubylulz@protonmail.com>
2021-05-03 00:34:17 +0200

    iterate on YeastBid contract

--- test/YeastBid.js
+++ test/YeastBid.js
@@ -1,17 +1,16 @@
-/*
-var YeastBid = artifacts.require("YeastBid");
+const YeastBid = artifacts.require("YeastBid");
 
-contract('YeastBid', function(accounts) {
-    var sb; // To store the instance when running
-
-    // Test case 1
-    it("Test initial balance", function() {
-        return SimpleBank.deployed().then(function(instance) {
-            sb = instance;
-            return sb.getBalance({ from: accounts[0] });
-        }).then(function(x) {
-            assert.equal(0, x, "Wrong initial balance");
-        });
+contract('YeastBid', () => {
+    it("Deployable test", async () => {
+        const my_contract = await YeastBid.deployed();
+        console.log(my_contract.address);
+        assert(my_contract.address != "");
     });
+
+    //it("Amount test", async () => {
+    //    const my_contract = await YeastBid.deployed();
+    //    const result = await my_contract.get_amount(); 
+    //    console.log(result);
+    //    assert(result == 100,result);
+    //});
 });
-*/
